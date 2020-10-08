fb9e9297583616821b7e0a680415c876d140ac99
palmandaniel <palman.daniel47@gmail.com>
2020-10-08 13:18:40 +0200

    caos

--- DHCPSzimulacio/Program.cs
+++ DHCPSzimulacio/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region more
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,26 +13,27 @@ namespace DHCPSzimulacio
         static List<string> excluded = new List<string>();
         static Dictionary<string, string> reserved = new Dictionary<string, string>();
         static Dictionary<string, string> dhcp = new Dictionary<string, string>();
-        static void BeolvasExcluded()
+        static List<string> commands = new List<string>();
+        static void BeolvasList(List<string> l, string filenev)
         {
             try
             {
-                StreamReader file = new StreamReader("excluded.csv");
+                StreamReader file = new StreamReader(filenev);
                 try
                 {
                     while (!file.EndOfStream)
                     {
-                        excluded.Add(file.ReadLine());
+                        l.Add(file.ReadLine());
                     }
 
                 }
                 catch (Exception exception)
                 {
-                        Console.WriteLine(exception.Message);
+                    Console.WriteLine(exception.Message);
                 }
                 finally
                 {
-                        file.Close();
+                    file.Close();
                 }
                 file.Close();
             }
@@ -53,17 +55,17 @@ namespace DHCPSzimulacio
              * 
              * összefűzni string-é
              */
-           
-                string[] adatok = cim.Split('.');
-                int okt4 = int.Parse(adatok[3]);
 
-                if (okt4 < 255)
-                {
-                    okt4++;
-                }
-                return adatok[0] + "." + adatok[1] + "." + adatok[2] + "." + okt4.ToString();
+            string[] adatok = cim.Split('.');
+            int okt4 = int.Parse(adatok[3]);
 
-         }
+            if (okt4 < 255)
+            {
+                okt4++;
+            }
+            return adatok[0] + "." + adatok[1] + "." + adatok[2] + "." + okt4.ToString();
+
+        }
 
         static void BeolvasDictionary(Dictionary<string, string> d, string filenev)
         {
@@ -84,32 +86,96 @@ namespace DHCPSzimulacio
             }
         }
 
-            static void Main(string[] args)
-            {
-                BeolvasExcluded();
-            BeolvasDictionary(dhcp, "dhcp.csv");
-            BeolvasDictionary(reserved, "reserved.csv");
+        static void Feladat(string parancs)
+        {
+            /*
+             * parancs = "request;D193123570A82"
+             * először csak "request" parancs
+             * megnezzuk hogy request-e
+             */
 
-            foreach (var e in dhcp)
+
+            if (parancs.Contains("request"))
             {
-                Console.WriteLine(e);
+                string[] a = parancs.Split(';');
+                string mac = a[1];
+
+                if (dhcp.ContainsKey(mac))
+                {
+                    Console.WriteLine($"DHCP {mac}-->{dhcp[mac]}");
+                }
+                else
+                {
+                    if (reserved.ContainsKey(mac))
+                    {
+                        Console.WriteLine($"Reserved {mac}-->{reserved[mac]}");
+                        dhcp.Add(mac, reserved[mac]);
+                    }
+
+                    else
+                    {
+                        string indulo = "192.168.10.100";
+                        int okt4 = 100;
+
+                        while (okt4 < 200 && (dhcp.ContainsValue(indulo) || reserved.ContainsValue(indulo) || excluded.Contains(indulo)))
+                        {
+                            okt4++;
+                            indulo = CimEggyelNo(indulo);
+                        }
+
+                        if (okt4 <200)
+                        {
+                            Console.WriteLine($"Kiosztott: {mac} --> {indulo}");
+                            dhcp.Add(mac, indulo);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine($"{mac} nincs IP!");
+                        }
+                    }
+                }
+
             }
-
-            foreach (var e in reserved)
+            else
             {
-                Console.WriteLine(e);
-            }
+                Console.WriteLine("nem oke");
 
-                //foreach (var e in excluded)
-                //{
-                //    Console.WriteLine(e);
-                //}
-
-            Console.WriteLine("\nVége");
-
-            Console.WriteLine(CimEggyelNo("192.168.10.100"));
-            Console.ReadKey();
             }
         }
 
+        static void Feladatok()
+        {
+            foreach (var command in commands)
+            {
+                Feladat(command);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            #region Beolvasasok
+            BeolvasList(excluded, "excluded.csv");
+            BeolvasList(commands, "test.csv");
+            BeolvasDictionary(dhcp, "dhcp.csv");
+            BeolvasDictionary(reserved, "reserved.csv");
+            #endregion
+
+
+
+            //foreach (var e in excluded)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            Feladatok();
+        
+        Console.WriteLine("\nVége");
+
+            Console.WriteLine(CimEggyelNo("192.168.10.100"));
+            Console.ReadKey();
+        }
     }
+
+} 
+#endregion
